cd2ad63f9070707916d488501950525594bf077b
Piatekf <filip.piatek2005@gmail.com>
2023-11-08 17:30:24 +0100

    Etap1 finished

--- Etap1/Form1.cs
+++ Etap1/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Etap1
 {
     public partial class Form1 : Form
@@ -5,6 +7,49 @@ namespace Etap1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void analyzeButton_Click(object sender, EventArgs e)
+        {
+            string baseText = hugeTextInput.Text;
+            bool isAnyLetter = false;
+            bool isAnyDigit = false;
+            bool isAnySpecialChar = false;
+
+            textLenInput.Text = baseText.Length.ToString();
+
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            foreach (char c in baseText)
+            {
+                if (!isAnyLetter && char.IsLetter(c))
+                {
+                    isAnyLetter = true;
+                }
+                else if (!isAnyDigit && char.IsDigit(c))
+                {
+                    isAnyDigit = true;
+                }
+                else if (!isAnySpecialChar && !char.IsLetterOrDigit(c) && !char.IsWhiteSpace(c))
+                {
+                    isAnySpecialChar = true;
+                }
+
+                if (isAnyDigit && isAnyLetter && isAnySpecialChar)
+                {
+                    break;
+                }
+
+            }
+
+            stopwatch.Stop();
+
+            isAnyDigitInput.Text = isAnyDigit.ToString();
+            isAnyLetterInput.Text = isAnyLetter.ToString();
+            isAnySpecialCharInput.Text = isAnySpecialChar.ToString();
+            timeElapsedInput.Text = stopwatch.Elapsed.ToString();
+
         }
     }
 }
